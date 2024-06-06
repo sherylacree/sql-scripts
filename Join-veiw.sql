aa88faed09bb06f744727fcf2abe89c0c9d01361
sheryl acree <sacree@tql.com>
2024-06-06 10:17:00 -0400

    Joined tables from Orders and Order Lines, sorted by Product and Product Description

---
+++ Join-veiw.sql
@@ -0,0 +1,81 @@
+﻿2018-11-22 00:00:00.000,My 2nd order,Book,Dress for Success,1,9.99
+2018-07-04 00:00:00.000,My 5st order,Book,Dress for Success,1,9.99
+2018-12-11 00:00:00.000,My 7th order,Book,Dress for Success,1,9.99
+2018-07-11 00:00:00.000,My 19th order,Book,Dress for Success,1,9.99
+2018-12-11 00:00:00.000,My 18th order,Book,Dress for Success,1,9.99
+2017-06-21 00:00:00.000,My 15th order,Book,Dress for Success,1,9.99
+2017-06-21 00:00:00.000,My 13th order,Book,Dress for Success,1,9.99
+2019-01-22 00:00:00.000,My 20th order,Book,Dress for Success,1,9.99
+2018-12-11 00:00:00.000,My 21st order,Book,Dress for Success,1,9.99
+2018-07-11 00:00:00.000,My 24th order,Book,Dress for Success,1,9.99
+2017-06-11 00:00:00.000,My 26th order,Book,Dress for Success,1,9.99
+2017-09-21 00:00:00.000,My 27th order,Book,Millionarie Next Door,1,19.99
+2018-12-11 00:00:00.000,My 25th order,Book,Millionarie Next Door,1,19.99
+2018-01-11 00:00:00.000,My 22nd order,Book,Millionarie Next Door,1,19.99
+2018-05-11 00:00:00.000,My 23nd order,Book,Millionarie Next Door,1,19.99
+2018-12-11 00:00:00.000,My 7th order,Book,Millionarie Next Door,1,19.99
+2017-06-21 00:00:00.000,My 11th order,Book,Millionarie Next Door,1,19.99
+2017-08-21 00:00:00.000,My 14th order,Book,Millionarie Next Door,1,19.99
+2017-06-11 00:00:00.000,My 17th order,Book,Millionarie Next Door,1,19.99
+2017-09-21 00:00:00.000,My 16th order,Book,Millionarie Next Door,1,19.99
+2019-01-22 00:00:00.000,My 1st order,Book,Millionarie Next Door,1,19.99
+2018-05-11 00:00:00.000,My 9th order,Book,Millionarie Next Door,1,19.99
+2019-01-22 00:00:00.000,My 6st order,Book,Millionarie Next Door,1,19.99
+2018-01-11 00:00:00.000,My 8th order,Book,Millionarie Next Door,1,19.99
+2019-01-02 00:00:00.000,My 4st order,Book,Millionarie Next Door,1,19.99
+2018-03-22 00:00:00.000,My 3rd order,Book,Millionarie Next Door,1,19.99
+2019-01-22 00:00:00.000,My 1st order,Book,Millionarie Next Door,1,19.99
+2019-01-22 00:00:00.000,My 1st order,Echo,Amazon Echo,1,99.99
+2018-11-22 00:00:00.000,My 2nd order,Echo,Amazon Echo,1,99.99
+2019-01-22 00:00:00.000,My 6st order,Echo,Amazon Echo,1,99.99
+2018-07-04 00:00:00.000,My 5st order,Echo,Amazon Echo,1,99.99
+2018-12-11 00:00:00.000,My 7th order,Echo,Amazon Echo,1,99.99
+2018-07-11 00:00:00.000,My 19th order,Echo,Amazon Echo,1,99.99
+2019-01-22 00:00:00.000,My 1st order,Echo,Amazon Echo,1,99.99
+2018-12-11 00:00:00.000,My 18th order,Echo,Amazon Echo,1,99.99
+2017-08-21 00:00:00.000,My 14th order,Echo,Amazon Echo,1,99.99
+2017-06-21 00:00:00.000,My 15th order,Echo,Amazon Echo,1,99.99
+2017-06-21 00:00:00.000,My 13th order,Echo,Amazon Echo,1,99.99
+2018-12-11 00:00:00.000,My 21st order,Echo,Amazon Echo,1,99.99
+2019-01-22 00:00:00.000,My 20th order,Echo,Amazon Echo,1,99.99
+2018-12-11 00:00:00.000,My 25th order,Echo,Amazon Echo,1,99.99
+2018-07-11 00:00:00.000,My 24th order,Echo,Amazon Echo,1,99.99
+2017-06-11 00:00:00.000,My 26th order,Echo,Amazon Echo,1,99.99
+2017-09-21 00:00:00.000,My 27th order,Echo Dot,Amazon Echo Dot,1,39.99
+2018-01-11 00:00:00.000,My 22nd order,Echo Dot,Amazon Echo Dot,1,39.99
+2018-12-11 00:00:00.000,My 7th order,Echo Dot,Amazon Echo Dot,1,39.99
+2017-06-11 00:00:00.000,My 17th order,Echo Dot,Amazon Echo Dot,1,39.99
+2018-01-11 00:00:00.000,My 8th order,Echo Dot,Amazon Echo Dot,1,39.99
+2018-03-22 00:00:00.000,My 3rd order,Echo Dot,Amazon Echo Dot,1,39.99
+2019-01-02 00:00:00.000,My 4st order,Tablet,Apple iPad,1,499.99
+2018-03-22 00:00:00.000,My 3rd order,Tablet,Apple iPad,1,499.99
+2018-01-11 00:00:00.000,My 8th order,Tablet,Apple iPad,1,499.99
+2017-09-21 00:00:00.000,My 16th order,Tablet,Apple iPad,1,499.99
+2018-05-11 00:00:00.000,My 9th order,Tablet,Apple iPad,1,499.99
+2017-06-11 00:00:00.000,My 17th order,Tablet,Apple iPad,1,499.99
+2018-12-11 00:00:00.000,My 7th order,Tablet,Apple iPad,1,499.99
+2017-06-21 00:00:00.000,My 11th order,Tablet,Apple iPad,1,499.99
+2018-05-11 00:00:00.000,My 23nd order,Tablet,Apple iPad,1,499.99
+2017-09-21 00:00:00.000,My 27th order,Tablet,Apple iPad,1,499.99
+2018-01-11 00:00:00.000,My 22nd order,Tablet,Apple iPad,1,499.99
+2018-12-11 00:00:00.000,My 25th order,Watch,Rolex,1,1999.99
+2017-08-21 00:00:00.000,My 14th order,Watch,Rolex,1,1999.99
+2019-01-22 00:00:00.000,My 6st order,Watch,Rolex,1,1999.99
+2019-01-22 00:00:00.000,My 1st order,Watch,Rolex,1,1999.99
+2019-01-22 00:00:00.000,My 1st order,Watch,Rolex,1,1999.99
+2018-11-22 00:00:00.000,My 2nd order,Watch,Timex,1,19.99
+2019-01-02 00:00:00.000,My 4st order,Watch,Timex,1,19.99
+2018-07-04 00:00:00.000,My 5st order,Watch,Timex,1,19.99
+2018-07-11 00:00:00.000,My 19th order,Watch,Timex,1,19.99
+2018-05-11 00:00:00.000,My 9th order,Watch,Timex,1,19.99
+2018-12-11 00:00:00.000,My 7th order,Watch,Timex,1,19.99
+2017-06-21 00:00:00.000,My 15th order,Watch,Timex,1,19.99
+2017-09-21 00:00:00.000,My 16th order,Watch,Timex,1,19.99
+2018-12-11 00:00:00.000,My 18th order,Watch,Timex,1,19.99
+2018-12-11 00:00:00.000,My 21st order,Watch,Timex,1,19.99
+2019-01-22 00:00:00.000,My 20th order,Watch,Timex,1,19.99
+2017-06-21 00:00:00.000,My 11th order,Watch,Timex,1,19.99
+2017-06-21 00:00:00.000,My 13th order,Watch,Timex,1,19.99
+2017-06-11 00:00:00.000,My 26th order,Watch,Timex,1,19.99
+2018-05-11 00:00:00.000,My 23nd order,Watch,Timex,1,19.99
+2018-07-11 00:00:00.000,My 24th order,Watch,Timex,1,19.99
